c6634caa498490ce0b2cddf1c7a6cc2cdeccc627
jufrancopy <jucfra23@gmail.com>
2021-10-01 09:30:47 -0400

    Actualización de CRUD Talento Humano con Datatables y SweetAlert2

--- app/Http/Controllers/Admin/Proyectos/EPC/TalentoHumanoController.php
+++ app/Http/Controllers/Admin/Proyectos/EPC/TalentoHumanoController.php
@@ -2,114 +2,58 @@
 
 namespace App\Http\Controllers\Admin\Proyectos\EPC;
 
-use App\Admin\Proyecto\EPC\Especialidad;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
 use DataTables;
-use Redirect, Response;
 
 use App\Admin\Proyecto\EPC\TalentoHumano;
-use Yajra\DataTables\Contracts\DataTable;
 
 class TalentoHumanoController extends Controller
 {
-public function index(Request $request)
-     {
-         $tthhs = TalentoHumano::item($request->input('item'))->get();
-
-         return view('admin.proyectos.epc.tthh.index', get_defined_vars())
-             ->with('i', ($request->input('page', 1) - 1) * 5);
-    }
-    // public function index(Request $request)
-    // {
-    //     if ($request->ajax()) {
-    //         $data = TalentoHumano::latest()->get();
-            
-    //         return DataTable::of($data)
-    //             ->addIndexColumn()
-    //             ->addColumn('action', function ($row) {
-
-    //                 $action = '<a class="btn btn-info" id="show-user" data-toggle="modal" data-id=' . $row->id . '>Show</a>
-    //     <a class="btn btn-success" id="edit-user" data-toggle="modal" data-id=' . $row->id . '>Edit </a>
-    //     <meta name="csrf-token" content="{{ csrf_token() }}">
-    //     <a id="delete-user" data-id=' . $row->id . ' class="btn btn-danger delete-user">Delete</a>';
-
-    //                 return $action;
-    //             })
-    //             ->rawColumns(['action'])
-    //             ->make(true);
-    //     }
-
-    //     return view('admin.proyectos.epc.tthh.index');
-    // }
-
-    public function getData()
+    public function index(Request $request)
     {
-        $query = TalentoHumano::select('id', 'item', 'type', 'cost', 'hours');
+        if ($request->ajax()) {
+            $data = TalentoHumano::latest()->get();
+            return Datatables::of($data)
+                ->addIndexColumn()
+                ->addColumn('action', function ($row) {
 
-        return datatables()->of(TalentoHumano::all())->toJson();
+                    $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="' . $row->id . '" data-original-title="Edit" class="edit btn btn-primary btn-sm editTalentoHumano">Editar</a>';
+                    $btn = $btn . ' <a href="javascript:void(0)" data-toggle="tooltip"  onclick="alertarEliminacion("data-id="' . $row->id . '"");"  data-original-title="Delete" class="btn btn-danger btn-sm deleteTalentoHumano">Borrar</a>';
+
+                    return $btn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+        return view('admin.proyectos.epc.tthh.index');
     }
-    public function get(Request $request)
-    {
-        $rows = TalentoHumano::where(DB::raw("lower(item)"), 'like', '%' . mb_strtolower($request->q) . '%')
-            ->select('item AS text', 'id')
-            ->orderBy('item')
-            ->limit(25)
-            ->get();
 
-        return response()->json($rows);
-    }
-
-    public function create()
-    {
-        $especialidades = Especialidad::orderBy('id', 'ASC')->pluck('item', 'id');
-
-        $espcialidadesChecked = [];
-
-        return view('admin.proyectos.epc.tthh.create', get_defined_vars());
-    }
 
     public function store(Request $request)
     {
-        TalentoHumano::create($request->all());
+        TalentoHumano::updateOrCreate([
+            'id'       => $request->tthh_id,
+            'item'     => $request->item,
+            'type'     => $request->type,
+            'hours'     => $request->hours,
+            'cost'     => $request->cost
+        ]);
 
-        return redirect()
-            ->route('proyectos-epc-tthh.index')
-            ->with('info', 'Ítem creado con éxito')
-            ->with('typealert', 'success');
-    }
 
-    public function show($id)
-    {
-        //
+        return response()->json(['info' => 'Item creado correctamente.', 'typealert' => 'success']);
     }
 
     public function edit($id)
     {
         $tthh = TalentoHumano::find($id);
-        $especialidades = Especialidad::orderBy('id', 'ASC')->pluck('item', 'id');
-
-        return view('admin.proyectos.epc.tthh.edit', get_defined_vars());
-    }
-
-    public function update(Request $request, $id)
-    {
-        $tthh = TalentoHumano::find($id);
-        $tthh->fill($request->all())->save();
-
-        return redirect()
-            ->route('proyectos-epc-tthh.index')
-            ->with('info', 'Ítem actualizado con éxito')
-            ->with('typealert', 'success');
+        return response()->json($tthh);
     }
 
     public function destroy($id)
     {
-        $tthh = TalentoHumano::find($id)->delete();
+        TalentoHumano::find($id)->delete();
 
-        return back()
-            ->with('info', 'Ítem eliminado con éxito')
-            ->with('typealert', 'success');
+        return response()->json(['success' => 'Permiso eliminado correctamente.']);
     }
 }
--- resources/views/admin/proyectos/epc/tthh/index.blade.php
+++ resources/views/admin/proyectos/epc/tthh/index.blade.php
@@ -1,100 +1,242 @@
 @extends('layouts.master')
 @section('content')
-<div class="content">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                @if ($message = Session::get('success'))
-                <div class="alert alert-info">
-                    <p>{{ $message }}</p>
+
+<div class="row">
+    <div class="col-md-12">
+        @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+        @endif
+        <div class="card">
+            <div class="card-header card-header-info">
+                <h4 class="card-title ">Gestionar Talentos Humanos</h4>
+
+                @can('role-create')
+                <a class="btn btn-success" href="javascript:void(0)" id="crearNuevoTalentoHumano"> Nuevo Ítem</a>
+                @endcan
+
+                <div class="pull-right">
+                    <a class="btn btn-warning pull-right" href="{{ route('globales-dashboard') }}"> Atras</a>
                 </div>
-                @endif
+
             </div>
-            <div class="card">
-                <div class="card-header card-header-info">
-                    <h4 class="card-title ">Talentos Humanos</h4>
-                    <a class="btn btn-success" href="{{ route('proyectos-epc-tthh.create') }}">Agregar</a>
-                    <div class="pull-right">
-                        <a class="btn btn-warning pull-right" href="{{ route('proyectos-epc-dashboard') }}"> Atras</a>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <table id="example" class="table table-striped table-bordered" style="width:100%">
+
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered data-table">
                         <thead>
                             <tr>
-                                <th>Nro</th>
-                                <th>Nombre</th>
-                                <th>Horas</th>
+                                <th>ID</th>
+                                <th>Item</th>
+                                <th>Tipo</th>
+                                <th>Hora</th>
                                 <th>Costo</th>
-                                <th width="280px">Action</th>
+                                <th width="280px">Acciones</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($tthhs as $key => $tthh)
-                            <tr>
-                                <td>{{ ++$i }}</td>
-                                <td>{{ $tthh->item }}</td>
-                                <td>{{ $tthh->hours }}</td>
-                                <td>{{ $tthh->cost }}</td>
-                                <td>
-                                    <a class="btn btn-primary btn-circle"
-                                        href="{{ route('proyectos-epc-tthh.edit', $tthh->id) }}">
-                                        <i class="far fa-edit"></i>
-                                    </a>
-
-                                    <a href="#" onclick="deleteConfirm('{{$tthh->id}}')">
-                                        <button class="btn btn-danger btn-circle">
-                                            <i class="fa fa-trash"></i>
-                                        </button>
-                                    </a>
-
-                                    {!! Form::open([
-                                    'route' => ['proyectos-epc-tthh.destroy',$tthh->id],
-                                    'method' => 'DELETE',
-                                    'id'=> $tthh->id,
-                                    'style'=>'display:inline']) !!}
-                                    {!! Form::close() !!}
-                                </td>
-                            </tr>
-                            @endforeach
                         </tbody>
                     </table>
                 </div>
             </div>
+
+
+            <div class="modal fade" id="ajaxModel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title" id="modelHeading"></h4>
+                        </div>
+                        <div class="modal-body">
+                            <form id="tthhForm" name="tthhForm" class="form-horizontal">
+                                <input type="hidden" name="tthh_id" id="tthh_id">
+                                <div class="form-group">
+                                    <label for="item" class="col-sm-4 control-label">Item</label>
+                                    <div class="col-sm-12">
+                                        <input type="text" class="form-control" id="item" name="item" value=""
+                                            maxlength="50" required="">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="type" class="col-sm-4 control-label">Type</label>
+                                    <div class="col-sm-12">
+                                        <input type="text" class="form-control" id="type" name="type" value=""
+                                            maxlength="50" required="">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="hours" class="col-sm-4 control-label">Hora</label>
+                                    <div class="col-sm-12">
+                                        <input type="text" class="form-control" id="hours" name="hours" value=""
+                                            maxlength="50" required="">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="cost" class="col-sm-4 control-label">Costo</label>
+                                    <div class="col-sm-12">
+                                        <input type="text" class="form-control" id="cost" name="cost" value=""
+                                            maxlength="50" required="">
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-offset-2 col-sm-10">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                                    <button type="submit" class="btn btn-success" id="saveBtn" value="create">Guardar
+                                        cambios
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
 </div>
-@endsection
 @section('scripts')
-<script>
-    $(document).ready(function() {
-        $('#example').DataTable({
-            // 'processing': true,
-            'serverSide': true,
-            'ajax': '{{ url('api/tthh') }}',
-            'columns': [
-                {data:'id'},
-                {data:'item'},
-                {data:'type'},
-                {data:'cost'},
-
-            ]
-        });
-    } );
-
-    window.deleteConfirm = function (formId) {
-        Swal.fire({
+<script type="text/javascript">
+    function alertarEliminacion(){
+            Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
             icon: 'warning',
-            text: 'Quieres borrar esto?',
             showCancelButton: true,
-            confirmButtonText: 'Borrar',
-            cancelButtonText: 'Cancelar',
-            confirmButtonColor: '#e3342f',
-        }).then((result) => {
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
             if (result.isConfirmed) {
-                document.getElementById(formId).submit()
+                Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+                )
+                }
+            })
+        }
+    $(function() {
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
         });
-    }
+
+        var table = $('.data-table').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{ route('proyectos-epc-tthh.index') }}",
+            columns: [{
+                data: 'DT_RowIndex',
+                name: 'DT_RowIndex'
+            }, 
+            {
+                data: 'item',
+                name: 'item'
+            }, 
+            {
+                data: 'type',
+                name: 'type'
+            },
+
+            {
+                data: 'hours',
+                name: 'hours'
+            },  
+            {
+                data: 'cost',
+                name: 'cost'
+            },  
+            {
+                data: 'action',
+                name: 'action',
+                orderable: false,
+                searchable: false
+            }, ]
+        });
+
+        $('#crearNuevoTalentoHumano').click(function() {
+            // $('#saveBtn').val("create-product");
+            $('#tthh_id').val('');
+            $('#modelHeading').html("Nuevo Item");
+            $('#ajaxModel').modal('show');
+        });
+
+        $('body').on('click', '.editTalentoHumano', function() {
+            var tthh_id = $(this).data('id');
+            $.get("{{ route('proyectos-epc-tthh.index') }}" + '/' + tthh_id + '/edit', function(data) {
+                $('#modelHeading').html("Editar Item");
+                // $('#saveBtn').val("edit-user");
+                $('#ajaxModel').modal('show');
+                $('#tthh_id').val(data.id);
+                $('#item').val(data.item);
+                $('#type').val(data.type);
+                $('#hours').val(data.hours);
+                $('#cost').val(data.cost);
+            })
+        });
+
+        $('#saveBtn').click(function(e) {
+            e.preventDefault();
+            $(this).html('Enviando...');
+
+            $.ajax({
+                data: $('#tthhForm').serialize(),
+                url: "{{ route('proyectos-epc-tthh.store') }}",
+                type: "POST",
+                dataType: 'json',
+                success: function(data) {
+
+                    $('#tthhForm').trigger("reset");
+                    $('#ajaxModel').modal('hide');
+                    table.draw();
+
+                },
+                error: function(data) {
+                    console.log('Error:', data);
+                    $('#saveBtn').html('Guardar Cambios');
+                }
+            });
+        });
+
+        $('body').on('click', '.deleteTalentoHumano', function() {
+
+            var tthh_id = $(this).data("id");
+            
+            Swal.fire({
+                title: 'Estás seguro de eliminar?',
+                text: "No podrás revertir esta acción",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                if (result.isConfirmed) {
+                    $.ajax({
+                    type: "DELETE",
+                    url: "{{ route('proyectos-epc-tthh.store') }}" + '/' + tthh_id,
+                    success: function(data) {
+                        table.draw();
+                        Swal.fire(
+                        'Borrado!',
+                        'El registro ha sido eliminado.',
+                        'success'
+                        )
+                    },
+                    error: function(data) {
+                        console.log('Error:', data);
+                    }
+                });
+                }
+            })
+        });
+    });
 </script>
+@endsection
 @endsection
