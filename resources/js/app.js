84f3725435cd39a2a730cc3d73f8f073d40fa0ea
jufrancopy <jucfra23@gmail.com>
2022-10-20 14:00:41 -0300

    Actualizacion de Plantilla

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,11 @@ require('./bootstrap');
 require('./modal');
 require('./completed');
 
-require('./completed');
 
 // global.$ = global.jQuery = require('jquery');
+require('datatables.net')();
+require('datatables.net-buttons/js/buttons.colVis.js')();
+require('datatables.net-buttons/js/buttons.html5.js')();
+require('datatables.net-buttons/js/buttons.print.js')();
 
 window.Swal = require('sweetalert2')
