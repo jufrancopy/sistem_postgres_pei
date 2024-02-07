ae676972f1f078df7935a4b798178b96802c145f
jufrancopy <jucfra23@gmail.com>
2024-02-07 15:43:58 -0300

    Desarolllo del Moudolo Gestion de Inmuebles

---
+++ database/migrations/2024_02_07_180113_localities.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('localities', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cod_dpto');
+            $table->string('desc_dpto');
+            $table->integer('cod_dist');
+            $table->string('desc_dist');
+            $table->integer('area');
+            $table->integer('cod_barrio_loc');
+            $table->string('desc_barrio_loc');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
