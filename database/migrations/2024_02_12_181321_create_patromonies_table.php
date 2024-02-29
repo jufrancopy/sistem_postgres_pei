fcca871e28d35fab6ccea03143c56b355867ef1d
jufrancopy <jucfra23@gmail.com>
2024-02-29 08:15:10 -0300

    Mejoras

--- database/migrations/2024_02_12_181321_create_patromonies_table.php
+++ database/migrations/2024_02_12_181321_create_patromonies_table.php
@@ -15,42 +15,42 @@ return new class extends Migration
     {
         Schema::create('patrimonies', function (Blueprint $table) {
             $table->id();
-            $table->string('type'); //BIEN INMUEBLE DE:
-            $table->integer('quantityAccountCurrent'); //CANTIDAD DE CTAS CTES
-            $table->string('detailLocation'); //LOTE/MA/DPTO.
-            $table->integer('estateQuantity'); //CANTIDAD  DE FINCAS
-            $table->string('department'); //DEPARTAMENTO
-            $table->string('city'); //Ciudad
-            $table->string('locality'); //DISTRITO - CIUDAD
-            $table->decimal('latitude'); //LATITUD 
-            $table->decimal('longitude'); //LONGITUD 
-            $table->string('locationAddress'); //UBICACIÓN
-            $table->string('infrastructureType'); //Tipo de Infraestructura
-            $table->text('description'); //UBICACIÓN
-            $table->string('registryNumber');  //FINCA MATRICULA Nº             
-            $table->integer('cadastralCurrentAccount');  //CTA. CTE. CTRAL.  Y/0 PADRON N° 
-            $table->integer('estateStatus'); //SITUACION ACTUAL DEL INMUEBLE
-            $table->date('startDateContract'); //Fecha de Inicio del Contrato
-            $table->date('endDateContract'); //Fecha Fin del Contrato 
-            $table->integer('committedInvestment'); //INVERSION COMPROMETIDA 
-            $table->integer('transfer'); //TRANFERENCIA   
-            $table->integer('balanceForTransfer'); //SALDO A TRANSFERIR 
-            $table->string('tenant'); //ARRENDATARIO
-            $table->string('rentAmount'); //CANON
-            $table->integer('rentAmountPeriod'); //PERIODO DEL CANON
-            $table->string('contractResolution'); //PERIODO VIGENTE-DESDE 
-            $table->string('contractNumber'); //NRO DE CONTRATO
-            $table->string('currentPeriodStart'); //PERIODO VIGENTE-DESDE 
-            $table->string('currentPeriodEnd'); //PERIODO VIGENTE-HASTA 
-            $table->string('statusDocumentation'); //TENENCIA DE TITULO
-            $table->float('landAreaMt2'); //SUPERFICIE TERRENO M2
-            $table->float('landAreaHectares'); //SUPERFICIE TERRENO HA 
-            $table->string('landSubArea'); //SUPERFICIE (SOLO PARA LOS SUB NUMEROS)
-            $table->string('builtAreaM2'); //SUPERFICIE EDIFICADA M2
-            $table->string('builtValueGs'); //Valor Edificado Gs.
-            $table->string('propertyValueGs'); //Valor Terreno Gs.
-            $table->string('totalValueGs'); //Valor Total.
-            $table->string('possessionRentWithoutTitle'); //Valor Total.
+            $table->text('type'); // BIEN INMUEBLE DE:
+            $table->string('quantity_account_current'); // CANTIDAD DE CTAS CTES
+            $table->string('detail_location'); // LOTE/MA/DPTO.
+            $table->string('estate_quantity'); // CANTIDAD  DE FINCAS
+            $table->string('department'); // DEPARTAMENTO
+            $table->string('city'); // Ciudad
+            $table->string('locality'); // DISTRITO - CIUDAD
+            $table->decimal('latitude'); // LATITUD 
+            $table->decimal('longitude'); // LONGITUD 
+            $table->string('location_address'); // UBICACIÓN
+            $table->string('infrastructure_type'); // Tipo de Infraestructura
+            $table->text('description'); // UBICACIÓN
+            $table->string('registry_number');  // FINCA MATRICULA Nº             
+            $table->string('cadastral_current_account');  // CTA. CTE. CTRAL.  Y/0 PADRON N° 
+            $table->string('estate_status'); // SITUACION ACTUAL DEL INMUEBLE
+            $table->string('start_date_contract'); // Fecha de Inicio del Contrato
+            $table->string('end_date_contract'); // Fecha Fin del Contrato 
+            $table->string('committed_investment'); // INVERSION COMPROMETIDA 
+            $table->string('transfer'); // TRANFERENCIA   
+            $table->string('balance_for_transfer'); // SALDO A TRANSFERIR 
+            $table->string('tenant'); // ARRENDATARIO
+            $table->string('rent_amount'); // CANON
+            $table->integer('rent_amount_period'); // PERIODO DEL CANON
+            $table->string('contract_resolution'); // PERIODO VIGENTE-DESDE 
+            $table->string('contract_number'); // NRO DE CONTRATO
+            $table->string('current_period_start'); // PERIODO VIGENTE-DESDE 
+            $table->string('current_period_end'); // PERIODO VIGENTE-HASTA 
+            $table->string('status_documentation'); // TENENCIA DE TITULO
+            $table->string('land_area_mt2'); // SUPERFICIE TERRENO M2
+            $table->string('land_area_hectares'); // SUPERFICIE TERRENO HA 
+            $table->string('land_sub_area'); // SUPERFICIE (SOLO PARA LOS SUB NUMEROS)
+            $table->string('built_area_m2'); // SUPERFICIE EDIFICADA M2
+            $table->string('built_value_gs'); // Valor Edificado Gs.
+            $table->string('property_value_gs'); // Valor Terreno Gs.
+            $table->string('total_value_gs'); // Valor Total.
+            $table->string('possession_rent_without_title'); // Valor Total.
 
             $table->timestamps();
         });
