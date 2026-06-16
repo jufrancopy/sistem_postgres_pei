5267fe04ef5ef33af55958c8d1ca7be89e27f091
jufrancopy <jucfra23@gmail.com>
2026-06-16 10:05:33 -0300

    fix: getUsers excluido del middleware role:Administrador para que Gestor pueda buscar usuarios en select2

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth', 'role:Administrador']);
+        $this->middleware(['auth', 'role:Administrador'])->except(['getUsers', 'getUser', 'getUsersForGroup']);
     }
 
     public function index(Request $request)
