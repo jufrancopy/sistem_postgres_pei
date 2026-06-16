6b6d81e27c223d238b96e02f1893f6764774ea63
jufrancopy <jucfra23@gmail.com>
2026-06-15 21:07:59 -0300

    feat: sidebar para roles Gestor y Colaborador de Actividades

--- resources/views/layouts/includes/sidebar_izq.blade.php
+++ resources/views/layouts/includes/sidebar_izq.blade.php
@@ -315,6 +315,44 @@
             </li>
         @endrole
 
+        {{-- Sidebar exclusivo para Gestor de Actividades --}}
+        @role('Gestor de Actividades')
+            <li class="nav-item {{ request()->is('admin/globales/activities') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('globales.activities.index') }}">
+                    <i class="material-icons">rocket_launch</i>
+                    <p>Actividades</p>
+                </a>
+            </li>
+            <li class="nav-item {{ request()->is('mis-actividades') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('globales.activities.mis-actividades') }}">
+                    <i class="material-icons">task_alt</i>
+                    <p>Mis Actividades</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('profile.edit') }}">
+                    <i class="material-icons">person</i>
+                    <p>Mi Perfil</p>
+                </a>
+            </li>
+        @endrole
+
+        {{-- Sidebar exclusivo para Colaborador de Actividades --}}
+        @role('Colaborador de Actividades')
+            <li class="nav-item {{ request()->is('mis-actividades') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('globales.activities.mis-actividades') }}">
+                    <i class="material-icons">dashboard</i>
+                    <p>Mis Actividades</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('profile.edit') }}">
+                    <i class="material-icons">person</i>
+                    <p>Mi Perfil</p>
+                </a>
+            </li>
+        @endrole
+
         {{-- Sidebar exclusivo para Analista de Planificación --}}
         @role('Analista de Planificación')
             <li class="nav-item active">
